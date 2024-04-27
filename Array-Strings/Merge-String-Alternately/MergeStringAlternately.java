ae7783e12d6bd99351325d527e5d6673b4b287c2
Shubham Nahar <shunahar@publicisgroupe.net>
2024-04-27 12:28:24 +0530

    Merge-String-Alternately

--- Array-Strings/Merge-String-Alternately/MergeStringAlternately.java
+++ Array-Strings/Merge-String-Alternately/MergeStringAlternately.java
@@ -6,18 +6,22 @@ class MergeStringAlternately{
 
         int i=0, j=0;
 
+        //We use this while look until word1 & word2 are of same length
         while(i<word1.length() && j<word2.length()){
             merge[i+j] = word1.charAt(i++);
-            merge[i+j] = word2.charAt(j++);
+            merge[i+j] = word2.charAt(j++); 
         }
 
+        //If word 1 legth is greater - then we use this while loop
         while(i<word1.length()){
             merge[i+j] = word1.charAt(i++);
         }
+
+        //If word 2 legth is greater - then we use this while loop
         while (i<word2.length()) {
             merge[i+j] = word2.charAt(j++);
         }
-        return new String(merge);
+        return new String(merge); //Converting Array to String
     }
     public static void main(String[] args) {
         String s1 = "abcd";
