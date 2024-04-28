02f1f3cfd0bafd0780da95fa5a20bf5db16cebd3
Shubham Nahar <shubh.nahar07@gmail.com>
2024-04-28 11:52:26 +0530

    Update IsSubSequence.java

--- Two-Pointers/Is-Subsequence/IsSubSequence.java
+++ Two-Pointers/Is-Subsequence/IsSubSequence.java
@@ -1,5 +1,6 @@
 public class IsSubSequence{
     public static boolean isSubsequence(String s1, String s2){
+      // Check if s2 is longer than s1, if so, call isSubsequence with the strings swapped
       if(s2.length()>s1.length()){
         return isSubsequence(s2, s1);
       }
@@ -7,14 +8,17 @@ public class IsSubSequence{
       int j = 0;
   
       while(i<s1.length() && j<s2.length()){
+        // If characters at current positions are equal, move both pointers forward
         if(s1.charAt(i) == s2.charAt(j)){
           i++;
           j++;
         }
+        // If characters are not equal, move pointer for s1 forward
         else{
           i++;
         }
       }
+      // Check if j has traversed all the elements in  s2 return true, else false
       return (j==s2.length());
     }
     public static void main(String[] args) {
