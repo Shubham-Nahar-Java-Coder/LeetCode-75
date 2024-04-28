e88fee1fb7c58d52ff654b4401b3ce301a586af9
Shubham Nahar <shubh.nahar07@gmail.com>
2024-04-28 15:13:48 +0530

    Update FindTheHighestAltitudeUsingPrefixSum.java

--- Prefix-Sum/Find-the-Highest-Altitude/FindTheHighestAltitudeUsingPrefixSum.java
+++ Prefix-Sum/Find-the-Highest-Altitude/FindTheHighestAltitudeUsingPrefixSum.java
@@ -3,14 +3,24 @@ import java.util.List;
 
 public class FindTheHighestAltitudeUsingPrefixSum {
     public static int largestAltitude(int[] gain) {
+        // Create a prefixList to store altitudes
         List<Integer> prefixList = new ArrayList<>();
+        
         int max = 0;
         int altitude = 0;
+        
+        // Add initial altitude of 0 to the prefixList
         prefixList.add(0);
+        
+        // Iterate over the gain array to calculate altitudes
         for (int i = 0; i < gain.length; i++) {
+            
             altitude = altitude + gain[i];
+            // Add the current altitude to the prefixList
             prefixList.add(altitude);
         }
+
+        // Iterate over the list to find the maximum altitude
         for (int i = 0; i < prefixList.size(); i++) {
             if (prefixList.get(i) > max) {
                 max = prefixList.get(i);
