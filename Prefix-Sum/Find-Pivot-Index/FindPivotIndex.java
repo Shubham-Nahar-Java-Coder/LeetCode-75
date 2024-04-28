50edbe5fb21ac14f8a5dd95cf7e11538eb820c3c
Shubham Nahar <shubh.nahar07@gmail.com>
2024-04-28 17:55:06 +0530

    Update FindPivotIndex.java

--- Prefix-Sum/Find-Pivot-Index/FindPivotIndex.java
+++ Prefix-Sum/Find-Pivot-Index/FindPivotIndex.java
@@ -6,9 +6,11 @@ public class FindPivotIndex {
 
         for (int i = 1; i < nums.length; i++) {
             total = total + nums[i];
+            // Calculate the prefix sum for the current index
             preSum[i] = preSum[i-1] + nums[i-1];            
         }
         for (int i = 0; i < nums.length; i++) {
+            // Check if the prefix sum at index i is equal to the sum of the elements to the right and left
             if(preSum[i] == total - preSum[i] - nums[i]){
                 return i;
             }            
