2d4279f7f5d17ec3f4d0c0edcb836cacd30839c9
Shubham Nahar <shunahar@publicisgroupe.net>
2024-05-11 21:25:04 +0530

    Added Array and String

--- Array-Strings/Increasing-Triplet-Subsequence/IncreasingTripletSubsequence.java
+++ Array-Strings/Increasing-Triplet-Subsequence/IncreasingTripletSubsequence.java
@@ -1,21 +1,33 @@
 class IncreasingTripletSubsequence {
     public static boolean increasingTriplet(int[] nums) {
 
+        // Check if the input array is null or has fewer than 3 elements
         if (nums == null || nums.length < 3) {
             return false;
         }
 
+        // Initialize two variables to store the smallest and second smallest elements
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
+        // Iterate through each element in the input array
         for (int num : nums) {
+            // If the current element is less than or equal to the smallest element found so
+            // far
             if (num <= first) {
                 first = num;
-            } else if (num <= second) {
+            }
+            // else, if the current element is less than or equal to the second smallest
+            // element found so far
+            else if (num <= second) {
                 second = num;
-            } else {
+            }
+            // If neither of the above conditions is met, it means we found an increasing
+            // triplet
+            else {
                 return true;
             }
         }
+        // If the loop completes without finding an increasing triplet, return false
         return false;
     }
 
