cda6a2dcee2cf605153c7c904e4c53bfec407133
Shubham Nahar <shunahar@publicisgroupe.net>
2024-05-11 22:06:58 +0530

    String Compression

--- Array-Strings/String-Compression/StringCompression.java
+++ Array-Strings/String-Compression/StringCompression.java
@@ -5,24 +5,32 @@ class StringCompression {
 
         int i = 1;
         int count = 1;
-
+        // Append the first character to the StringBuilder
         sb.append(chars[0]);
 
+        // Iterate through the array starting from index 1
         while (i < chars.length) {
+            // If the current character is the same as the previous one
             if (chars[i] == chars[i - 1]) {
-                count++;
-            } else {
+                count++; // Increment the counter
+            }
+            // If the current character is different from the previous one
+            else {
+                // Append the count of the previous character if it's greater than 1
                 if (count > 1) {
                     sb.append(count);
                 }
+                // Append the current character
                 sb.append(chars[i]);
-                count = 1;
+                count = 1; // Reset the counter
             }
             i++;
         }
+        // Append the count of the last character if it's greater than 1
         if (count > 1) {
             sb.append(count);
         }
+        // Copy the compressed characters back to the original array 'chars'
         for (int j = 0; j < sb.length(); j++) {
             chars[j] = sb.charAt(j);
         }
